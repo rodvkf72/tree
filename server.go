bd8b15cd1aee2107de4abd9de22f4700fb1576d2
Kim Kwang Ho <rodvkf72@naver.com>
2023-03-24 15:05:39 +0900

    Update: Basic Log
    url 접속 시 기본적인 로깅 처리 (시간, url, ip 등)

--- server.go
+++ server.go
@@ -2,15 +2,44 @@ package main
 
 import (
 	"net/http"
+	"time"
+	"os"
+	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 
 	"tree/controller"
 )
 
 func main() {
 	e := echo.New()
+
+	now := time.Now()
+	custom := now.Format("2006-01-02")
+	fileName := custom + "_log.txt"
+
+	if _, err := os.Stat("./logs"); err != nil {	//폴더가 존재하지 않는 경우
+		os.MkdirAll("./logs", os.ModePerm)
+	}
+
+	f, file := os.OpenFile("./logs/" + fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if file != nil {
+		panic(fmt.Sprintf("error opening file : %v", file))
+	}
+
+	defer f.Close()
+
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: `{"time":"${time_rfc3339}", "remote_ip":"${remote_ip}", ` +
+			`"host":"${host}", "method":"${method}", "uri":"${uri}", "user_agent":"${user_agent}",` +
+			`"status":${status}, ` + "\n",
+		Output: f,
+	}))
+
+	e.Use(middleware.Recover()) //미들웨어에서 복구를 사용
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Tree-Api World!")
